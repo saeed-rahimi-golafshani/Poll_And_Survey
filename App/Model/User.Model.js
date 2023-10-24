7b2570082a2a7a972330446ff2522854dad076c2
saied <saiedrahimigolafshani@gmail.com>
2023-10-24 17:44:48 +0330

    UserModel

---
+++ App/Model/User.Model.js
@@ -0,0 +1,17 @@
+const { default: mongoose } = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  role_Id: {type: mongoose.Types.ObjectId, ref: "role"},
+  firstname: {type: String},
+  lastname: {type: String},
+  username: {type: String},
+  mobile: {type: String, required: true},
+  email: {type: String, lowercase: true},
+  birthday: {type: String},
+  active: {type: Boolean, default: true},
+  CreatedAt: {type: String},
+  UpdatedAt: {type: String},
+  Profile: {type: String},
+  introduction: {type: String},
+  host: {type: Boolean},
+})
