bac43cee27c1cef5e8283539fa959898f0eb2499
saied <saiedrahimigolafshani@gmail.com>
2023-10-28 23:12:35 +0330

    Token_signRefreshToken

--- App/Utills/Token.js
+++ App/Utills/Token.js
@@ -1,8 +1,9 @@
 const jwt = require("jsonwebtoken")
 const { Promise } = require("mongoose")
 const { UserModel } = require("../Model/User.Model");
-const { ACCESS_Token_SECRETKEY } = require("./Contents");
+const { ACCESS_Token_SECRETKEY, REFRESH_TOKEN_SECRETKEY } = require("./Contents");
 const createHttpError = require("http-errors");
+const redisClient = require("./Init_Redis");
 
 function signAccessToken(userId){
   return new Promise(async (resolve, reject) => {
@@ -21,10 +22,22 @@ function signAccessToken(userId){
 };
 function signRefreshToken(userId){
   return new Promise(async(resolve, reject) => {
-    
+    const user = await UserModel.findById(userId);
+    const payload = {
+      mobile: user.mobile
+    };
+    const option = {
+      expiresIn: "1y"
+    };
+    jwt.sign(payload, REFRESH_TOKEN_SECRETKEY, option, async(error, token) => {
+      if(error) reject(createHttpError.InternalServerError("خطای سروری"));
+      await redisClient.SETEX(userId, (360*24*60*60), token);
+      resolve(token)
+    })
   })
 }
 
 module.exports = {
-  signAccessToken
+  signAccessToken,
+  signRefreshToken
 }
